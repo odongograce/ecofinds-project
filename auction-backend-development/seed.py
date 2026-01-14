702cf3335fc7342be2dd964ba11d21a4a2eeef7a
Natalie Mwiti <kendimwiti@icloud.com>
2026-01-14 23:16:51 +0300

    Created users list2

--- auction-backend-development/seed.py
+++ auction-backend-development/seed.py
@@ -15,3 +15,8 @@ with app.app_context():
     bob = User(name="Bob", email="bob10101@gmail.com")
     carol = User(name="Carol", email="carol.okoth@gmail.com")
     sarah = User(name="Sarah", email="sarah.wavy@gmail.com")
+    esther = User(name="Esther", email="esther.bobo@gmail.com")
+    mark = User(name="Mark", email="markkamau@gmail.com")
+    ethan = User(name="Ethan", email="ethanruto@gmail.com")
+    kevin = User(name="Kevin", email="omondikevo@yahoo.com.au")
+    users = [george, bob, carol, sarah, esther, mark, ethan, kevin]
