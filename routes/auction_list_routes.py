edfeb5e4b61a1644d7f78e2c013b0ec54c7f04b7
Liban Golo <liban.golo@student.moringaschool.com>
2026-01-14 22:55:12 +0300

    auction list route done

--- routes/auction_list_routes.py
+++ routes/auction_list_routes.py
@@ -1,17 +1,18 @@
-from flask import Blueprint, request, jsonify
+from flask import request, jsonify
 from models import db, AuctionList
 
-auction_list_bp = Blueprint("auction_list", __name__, url_prefix="/auction-list")
+def register_auction_list_routes(app):
 
+    @app.route('/auction_list', methods=['GET'])
+    def get_auction_list():
+        auctions = AuctionList.query.all()
+        result = [{"id": a.id, "user_id": a.user_id, "auction_item_id": a.auction_item_id} for a in auctions]
+        return jsonify(result)
 
-@auction_list_bp.route("", methods=["POST"])
-def add_to_list():
-    data = request.get_json()
-    item = AuctionList(
-        user_id=data["user_id"],
-        auction_item_id=data["auction_item_id"],
-        added_at=data.get("added_at")
-    )
-    db.session.add(item)
-    db.session.commit()
-    return jsonify({"id": item.id}), 201
+    @app.route('/auction_list', methods=['POST'])
+    def add_to_auction_list():
+        data = request.get_json()
+        new_entry = AuctionList(user_id=data['user_id'], auction_item_id=data['auction_item_id'])
+        db.session.add(new_entry)
+        db.session.commit()
+        return jsonify({"message": "Added to auction list"}), 201
