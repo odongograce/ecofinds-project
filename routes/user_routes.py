b56f01228944de276dae9019d82ccc63a273c72f
Liban Golo <liban.golo@student.moringaschool.com>
2026-01-14 22:45:10 +0300

    changes to user routes

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,64 +1,19 @@
-from flask import Blueprint, request, jsonify
+
+from flask import request, jsonify
 from models import db, User
 
-user_bp = Blueprint("users", __name__, url_prefix="/users")
+def register_user_routes(app):
 
+    @app.route('/users', methods=['GET'])
+    def get_users():
+        users = User.query.all()
+        result = [{"id": u.id, "username": u.username, "email": u.email} for u in users]
+        return jsonify(result)
 
-@user_bp.route("", methods=["POST"])
-def create_user():
-    data = request.get_json()
-    new_user = User(
-        username=data["username"],
-        email=data["email"],
-        password=data["password"],
-        role=data.get("role", "user")
-    )
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({
-        "id": new_user.id,
-        "username": new_user.username,
-        "email": new_user.email,
-        "role": new_user.role
-    }), 201
-
-
-@user_bp.route("", methods=["GET"])
-def get_users():
-    users = User.query.all()
-    return jsonify([{
-        "id": u.id,
-        "username": u.username,
-        "email": u.email,
-        "role": u.role
-    } for u in users]), 200
-
-
-@user_bp.route("/<int:user_id>", methods=["GET"])
-def get_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return jsonify({
-        "id": user.id,
-        "username": user.username,
-        "email": user.email,
-        "role": user.role
-    }), 200
-
-
-@user_bp.route("/<int:user_id>", methods=["PUT"])
-def update_user(user_id):
-    user = User.query.get_or_404(user_id)
-    data = request.get_json()
-    user.username = data.get("username", user.username)
-    user.email = data.get("email", user.email)
-    user.role = data.get("role", user.role)
-    db.session.commit()
-    return jsonify({"message": "User updated successfully"}), 200
-
-
-@user_bp.route("/<int:user_id>", methods=["DELETE"])
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({"message": "User deleted"}), 200
+    @app.route('/users', methods=['POST'])
+    def create_user():
+        data = request.get_json()
+        new_user = User(username=data['username'], email=data['email'], password=data['password'])
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message": "User created"}), 201
